389ea9c37859a814bc0f8d1639c74ac579104784
percival <percival.engineer@gmail.com>
2023-08-20 09:51:15 -0600

    se cambio direccion de imagen graciosa

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 document.getElementById('siguiente').addEventListener('click', function() {
     const imagen = document.createElement('img');
-    imagen.src = "../imagen2.jpg"; // Cambia a la ruta de tu segunda imagen
+    imagen.src = "imagen2.jpg"; // Cambia a la ruta de tu segunda imagen
     imagen.alt = "segundo acto";
     document.body.appendChild(imagen);
 });
