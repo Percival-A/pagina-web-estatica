46428e78ec1320bccf34f552b0045b22389f97ec
percival <percival.engineer@gmail.com>
2023-08-20 09:33:43 -0600

    se agrega imagen graciosa

--- public/script.js
+++ public/script.js
@@ -1,3 +1,6 @@
-document.getElementById('saludar').addEventListener('click', function() {
-    alert('¡Hola Mundo!');
+document.getElementById('siguiente').addEventListener('click', function() {
+    const imagen = document.createElement('img');
+    imagen.src = "../imagen2.jpg"; // Cambia a la ruta de tu segunda imagen
+    imagen.alt = "segundo acto";
+    document.body.appendChild(imagen);
 });
